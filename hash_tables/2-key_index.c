698fb3350167159e3cf9a0ecaf404f44eaad504a
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-01 11:46:17 +0000

    feat(hash_tables): implement key_index using djb2 (RaghadNasiaf)

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,14 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index - gives the index of a key
- * @key: the key (as unsigned char *)
+ * key_index - compute index for a given key using djb2
+ * @key: key string (as unsigned char *)
  * @size: size of the hash table array
- * Return: index at which the key/value pair should be stored
+ *
+ * Return: index in range [0, size - 1]
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	/* size > 0 per project contract; return 0 defensively if misused */
+	/* حماية بسيطة لو تم تمرير حجم 0 بالخطأ */
 	if (size == 0)
 		return (0);
 
