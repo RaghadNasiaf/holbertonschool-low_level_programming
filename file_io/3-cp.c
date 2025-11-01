f4ba194079830829577fc285ef76563263c9ed7a
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-01 09:25:32 +0000

    file_io: refactor cp into helper functions to satisfy Betty (final clean version)

---
+++ file_io/3-cp.c
@@ -0,0 +1,89 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void close_fd(int fd);
+void copy_file(const char *file_from, const char *file_to);
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copy_file(argv[1], argv[2]);
+	return (0);
+}
+
+/**
+ * copy_file - copies the content of one file to another
+ * @file_from: source file name
+ * @file_to: destination file name
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to, r, w;
+	char buf[1024];
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		close_fd(fd_from);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buf, sizeof(buf))) > 0)
+	{
+		w = write(fd_to, buf, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+}
+
+/**
+ * close_fd - closes a file descriptor safely
+ * @fd: file descriptor to close
+ */
+void close_fd(int fd)
+{
+	int c = close(fd);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
