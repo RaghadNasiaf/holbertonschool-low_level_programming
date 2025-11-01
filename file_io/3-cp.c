c27f48d6d4f0bae385a9d7c81e26aee1008026fc
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-01 11:04:28 +0000

    style: split long function to satisfy Betty (RaghadNasiaf)

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define BUF_SIZE 1024
+
 /**
  * close_fd - closes a file descriptor safely
  * @fd: file descriptor to close
@@ -19,6 +21,40 @@ void close_fd(int fd)
 }
 
 /**
+ * open_src - open source file or exit(98)
+ * @file_from: source path
+ * Return: fd of source
+ */
+int open_src(const char *file_from)
+{
+	int fd_from = open(file_from, O_RDONLY);
+
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	return (fd_from);
+}
+
+/**
+ * open_dst - open destination file or exit(99)
+ * @file_to: destination path
+ * Return: fd of destination
+ */
+int open_dst(const char *file_to)
+{
+	int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	return (fd_to);
+}
+
+/**
  * copy_file - copies the content of one file to another
  * @file_from: source file name
  * @file_to: destination file name
@@ -26,16 +62,12 @@ void close_fd(int fd)
 void copy_file(const char *file_from, const char *file_to)
 {
 	int fd_from, fd_to, r, w;
-	char buf[1024];
+	char buf[BUF_SIZE];
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+	fd_from = open_src(file_from);
 
-	r = read(fd_from, buf, 1024);
+	/* probe read first to guarantee read-failure returns 98 before opening dst */
+	r = read(fd_from, buf, BUF_SIZE);
 	if (r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
@@ -43,13 +75,7 @@ void copy_file(const char *file_from, const char *file_to)
 		exit(98);
 	}
 
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		close_fd(fd_from);
-		exit(99);
-	}
+	fd_to = open_dst(file_to);
 
 	while (r > 0)
 	{
@@ -62,7 +88,7 @@ void copy_file(const char *file_from, const char *file_to)
 			exit(99);
 		}
 
-		r = read(fd_from, buf, 1024);
+		r = read(fd_from, buf, BUF_SIZE);
 		if (r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
