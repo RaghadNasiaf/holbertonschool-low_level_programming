b1124bf15623668d20bf93e0b6ac0985cf0785ae
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-05 17:07:10 +0000

    feat: add task 6 (print_line) with proper prototypes and README

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,9 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
+
+/* Task 3 */
 void print_numbers(void);
+
+/* Task 4 */
 void print_most_numbers(void);
+
+/* Task 5 */
 void more_numbers(void);
+
+/* Task 6 */
+void print_line(int n);
+
 #endif /* MAIN_H */
-void print_most_numbers(void);
-void more_numbers(void);
