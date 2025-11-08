551dd01a8277afa00100ceca3051434f0ee167f9
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-08 17:33:25 +0000

    fix: Betty style for str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -5,37 +5,36 @@
  * str_concat - concatenates two strings
  * @s1: first string
  * @s2: second string
- *
- * Return: pointer to new string, or NULL on failure
+ * Return: pointer to new string, or NULL if fails
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *s;
-    int i, j, len1 = 0, len2 = 0;
+	int i = 0, j = 0, len1 = 0, len2 = 0;
+	char *str;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    while (s1[len1] != '\0')
-        len1++;
+	while (s1[len1] != '\0')
+		len1++;
 
-    while (s2[len2] != '\0')
-        len2++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    s = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (s == NULL)
-        return (NULL);
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (str == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        s[i] = s1[i];
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
 
-    for (j = 0; j < len2; j++)
-        s[i + j] = s2[j];
+	for (j = 0; j < len2; j++)
+		str[i + j] = s2[j];
 
-    s[i + j] = '\0';
+	str[i + j] = '\0';
 
-    return (s);
+	return (str);
 }
 
