c624395b66a84642efa1157e9430cddda27d1578
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-08 19:42:36 +0000

    feat(variadic_functions): implement sum_them_all and header

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/* Prototypes */
+int sum_them_all(const unsigned int n, ...);
+
+/* Provided by checker (declare only) */
+int _putchar(char c);
+
+#endif /* VARIADIC_FUNCTIONS_H */
