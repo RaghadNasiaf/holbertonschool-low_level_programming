e4fea7bd9b60f7f768c5c6111898156c6f58dcea
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-08 18:38:05 +0000

    fix(structures_typedef): correct init_dog with proper NULL handling and betty format

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -4,9 +4,9 @@
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: pointer to struct dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,4 +14,13 @@ struct dog
 	char *owner;
 };
 
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
