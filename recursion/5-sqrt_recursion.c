890363b29c7c8178ca68989f32ce6a94a441d3f2
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-08 16:27:27 +0000

    recursion: implement _sqrt_recursion (task 5)

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * find_sqrt - helper function to find natural sqrt
+ * @n: number
+ * @i: current guess
+ *
+ * Return: sqrt or -1
+ */
+int find_sqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (find_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural sqrt of n
+ * @n: number
+ *
+ * Return: sqrt or -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (find_sqrt(n, 0));
+}
