9af7a00b19c7e15a8db50b4ed707d1e2ddaf5baf
RaghadNasiaf <naseefraghad@gmail.com>
2025-11-09 06:51:21 +0000

    Fix: use tabs for indentation in add_dnodeint_end (Betty)

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,38 +2,39 @@
 
 /**
  * add_dnodeint_end - adds a new node at the end of a dlistint_t list
- * @head: address of pointer to head node
- * @n: value for the new node
+ * @head: pointer to pointer to the head of the list
+ * @n: integer to store in the new node
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: address of the new element, or NULL if it fails
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *tail;
+	dlistint_t *new_node;
+	dlistint_t *temp;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->next = NULL;
+	new_node->n = n;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        new_node->prev = NULL;
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-    tail = *head;
-    while (tail->next != NULL)
-        tail = tail->next;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-    tail->next = new_node;
-    new_node->prev = tail;
+	temp->next = new_node;
+	new_node->prev = temp;
 
-    return (new_node);
+	return (new_node);
 }
